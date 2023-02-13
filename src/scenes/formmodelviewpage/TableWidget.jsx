7d0b462278515aba4c1ba5fc366fea26511e305e
Leo <leo56029132@gmail.com>
2023-02-13 13:22:01 +0800

    formmodelviewpage table replaceall cancel

--- src/scenes/formmodelviewpage/TableWidget.jsx
+++ src/scenes/formmodelviewpage/TableWidget.jsx
@@ -686,7 +686,7 @@ export const TableWidget = forwardRef(({
                                 }else if (field !== "blank"){
                                     var cell = (<span 
                                         className="CkeditorInput"
-                                        dangerouslySetInnerHTML={createMarkup(data[key].replaceAll('\n','<br>'))}
+                                        dangerouslySetInnerHTML={createMarkup(data[key])}
                                     >
                                     </span>)
                                 }
