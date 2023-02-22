71dbd68c013be7f34109c2414a8c3a62dc491670
Leo <leo56029132@gmail.com>
2023-02-22 13:48:21 +0800

    update

--- src/state/store.js
+++ src/state/store.js
@@ -102,9 +102,9 @@ export const FetchToStore = ({children}) =>{
             getAllFormData()
         }
         // Set interval to fetch data every 10 seconds
-        const intervalId = setInterval(() => {
-            getAllFormData()
-        }, 60000);
+        // const intervalId = setInterval(() => {
+        //     getAllFormData()
+        // }, 60000);
     
         // Clean up interval on unmount
         return () => clearInterval(intervalId);
